826441723125683efd3a5f0a29958125ad7b79e2
Matthew Berman <mberman84@gmail.com>
2011-10-11 22:13:51 -0700

    changed user edit fields - still need to fix user creation since password isn't allowed

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,11 +1,24 @@
-ActiveAdmin.register User do
+ActiveAdmin.register User do  
   index do
     column :id
     column :name, :sortable => :name do |user|
       link_to user.name, admin_user_path(user)
     end
     column :email
+    column :admin
     column :time_zone
+    column :location
     default_actions
+  end
+  
+  form do |f|
+    f.inputs "User Details" do
+      f.input :name
+      f.input :email
+      f.input :admin
+      f.input :time_zone
+      f.input :location
+    end
+    f.buttons
   end
 end
