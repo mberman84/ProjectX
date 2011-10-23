01866f303d3b2eadc5ca494eed000fa792b9929d
Matthew Berman <mberman84@gmail.com>
2011-10-22 21:58:21 -0700

    added access control tests for events_controller

---
+++ spec/controllers/events_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe EventsController do
+  render_views
+  
+  describe "access control" do
+    it "should deny access to 'create'" do
+      post :create
+      response.should redirect_to(signin_path)
+    end
+
+    it "should deny access to 'destroy'" do
+      delete :destroy, :id => 1
+      response.should redirect_to(signin_path)
+    end
+  end
+end  
