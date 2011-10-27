135c63ff4afdd7b7b15438a8f513426543df2756
Matthew Berman <mberman84@gmail.com>
2011-10-26 21:09:54 -0700

    added js following and tests for them

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -27,6 +27,13 @@ describe RelationshipsController do
         response.should redirect_to(user_path(@followed))
       end.should change(Relationship, :count).by(1)
     end
+    
+    it "should create a relationship with Ajax" do
+      lambda do
+        xhr :post, :create, :relationship => { :followed_id => @followed }
+        response.should be_success
+      end.should change(Relationship, :count).by(1)
+    end
   end
 
   describe "DELETE 'destroy'" do
@@ -42,6 +49,13 @@ describe RelationshipsController do
       lambda do
         delete :destroy, :id => @relationship
         response.should redirect_to(user_path(@followed))
+      end.should change(Relationship, :count).by(-1)
+    end
+    
+    it "should destroy a relationship with Ajax" do
+      lambda do
+        xhr :delete, :destroy, :id => @relationship
+        response.should be_success
       end.should change(Relationship, :count).by(-1)
     end
   end
