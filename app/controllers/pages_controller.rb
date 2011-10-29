9d89067ffd292cda1784bc57f241159a9dd7ef74
Matthew Berman <mberman84@gmail.com>
2011-10-29 13:55:52 -0700

    parsed event_feed on homepage by past events and upcoming events

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,10 @@ class PagesController < ApplicationController
     #@upcoming_events = Event.where(:event_date => Time.now..Time.now + 1.week).limit(10).find(:all, :order => "event_date")
     if signed_in?
       @event_feed = current_user.event_feed
+      @past_events = @event_feed.where("event_date < ?", Time.now)
+                                .limit(10).find(:all, :order => "event_date DESC")
+      @upcoming_events = @event_feed.where("event_date >= ?", Time.now)
+                                    .limit(10).find(:all, :order => "event_date")
     end
   end
 
