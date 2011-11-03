19bb34bff80a7762a40eab14c5a62eb801b3e000
Matthew Berman <mberman84@gmail.com>
2011-11-02 19:52:48 -0700

    added sidebar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,10 +7,10 @@ class PagesController < ApplicationController
       @json = Event.all.to_gmaps4rails
       @event_feed = current_user.event_feed
       @past_events = @event_feed.where("event_date < ?", Time.now)
-                                .limit(10)
+                                .limit(3)
                                 .find(:all, :order => "event_date DESC")
       @upcoming_events = @event_feed.where("event_date >= ?", Time.now)
-                                    .limit(10).find(:all, :order => "event_date")
+                                    .limit(3).find(:all, :order => "event_date")
     end
   end
 
