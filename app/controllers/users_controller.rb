4850077dcebbd5b5a0c4c3b8c0e20999af1d8a86
Matthew Berman <mberman84@gmail.com>
2011-11-06 14:57:39 -0800

    removed edit user function for admin users who aren't correct_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,7 +71,7 @@ class UsersController < ApplicationController
     
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user) || current_user.admin?
+      redirect_to(root_path) unless current_user?(@user)
     end
     
     def admin_user
