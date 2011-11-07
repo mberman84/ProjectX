39889e2fefc19db1892fe4d38359f7f069f10ce9
Matthew Berman <mberman84@gmail.com>
2011-11-06 19:44:42 -0800

    added unfollow to user list. removed ajax from follow/unfollow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,19 +2,21 @@ class RelationshipsController < ApplicationController
   before_filter :authenticate
   
   def create
+    session[:return_to] = request.referrer
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
     respond_to do |format|
-      format.html { redirect_to @user }
+      format.html { redirect_to session[:return_to] }
       format.js
     end
   end
   
   def destroy
+    session[:return_to] = request.referrer
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
     respond_to do |format|
-      format.html { redirect_to @user }
+      format.html { redirect_to session[:return_to] }
       format.js
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
-  before_filter :correct_user, :only => [:edit, :update, :show]
+  before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user,   :only => [:destroy]
   before_filter :not_signed_in, :only => [:new, :create]
   
