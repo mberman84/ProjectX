b0d4a91a3bf3e1143a8bb14ef0765ea87291c533
Matthew Berman <mberman84@gmail.com>
2011-11-12 11:38:50 -0800

    fixed bugs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
       render 'new'
     else
       sign_in user
-      redirect_back_or events
+      redirect_back_or events_path
     end                          
   end
   
