ec0f8bfaf10847b469abe877d9299e48599529a6
Matthew Berman <mberman84@gmail.com>
2011-11-13 17:15:15 -0800

    separating fb auth for dev and prod

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,9 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '223287611071138', '157a0139342acc03aea261a37ac47e78'
+if Rails.env == "production"
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, '109125412535038', '132b5c601566dcbbf531e9576600dca7'
+  end
+else
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, '223287611071138', '157a0139342acc03aea261a37ac47e78'
+  end
 end
