76a9134277a734770413fccbdf8679b46b8f724c
Matthew Berman <mberman84@gmail.com>
2011-11-13 19:46:28 -0800

    removed requirement for images for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -27,7 +27,7 @@ class Event < ActiveRecord::Base
                     :path           => "/:id/:style/:basename.:extension",
                     :storage        => :s3,
                     :s3_credentials => "#{RAILS_ROOT}/config/s3.yml"
-  validates_attachment_presence     :photo
+  #validates_attachment_presence     :photo
   validates_attachment_size         :photo, :less_than    => 5.megabytes
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 
                                                               'image/png', 
